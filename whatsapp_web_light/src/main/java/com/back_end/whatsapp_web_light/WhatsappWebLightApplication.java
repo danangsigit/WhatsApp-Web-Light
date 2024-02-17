29b3d36adb5c9cc55b59e750c8de8d74bec99a8e
Vítor Viana <vitorviana@id.uff.br>
2024-02-16 23:39:10 -0300

    Inicializando projeto Spring Boot

---
+++ whatsapp_web_light/src/main/java/com/back_end/whatsapp_web_light/WhatsappWebLightApplication.java
@@ -0,0 +1,13 @@
+package com.back_end.whatsapp_web_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappWebLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappWebLightApplication.class, args);
+	}
+
+}
---
+++ whatsapp_web_light/src/test/java/com/back_end/whatsapp_web_light/WhatsappWebLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_end.whatsapp_web_light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappWebLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
