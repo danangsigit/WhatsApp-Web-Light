178a31e8d2637f12248ade80deec78f6ad6b84f4
Vítor Viana <vitorviana@id.uff.br>
2024-02-19 16:38:28 -0300

    Put mapping user

--- whatsapp_web_light/src/main/java/com/back_end/whatsapp_web_light/controller/UserController.java
+++ whatsapp_web_light/src/main/java/com/back_end/whatsapp_web_light/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class UserController {
         return userService.findById(id);
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
     public UserResponseDTO update (@RequestBody @Valid UserRequestUpdateDTO userRequestUpdateDTO) {
         // Nome do usuario logado
         String user_name = "Vítor";
